a7cbb13506c2cec3b5f8b5eb472c28dcf7e1d019
lroygard <lroygard@gmail.com>
2024-10-02 22:37:08 +1300

    fixed backend whitespace

--- SPEED-app/SPEED-backend/src/app.module.ts
+++ SPEED-app/SPEED-backend/src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookModule } from './api/books/book.module';
 import { ConfigModule } from '@nestjs/config';
+
 @Module({
-imports: [
-ConfigModule.forRoot(),
-MongooseModule.forRoot(process.env.DB_URI),
-BookModule,
-],
-controllers: [AppController],
-providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DB_URI),
+        BookModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
